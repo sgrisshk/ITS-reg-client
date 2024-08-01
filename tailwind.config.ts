b9a2bc48fc016ed2e0053325abfe183e85d403a8
Sofya Grishkova <144588966+niigheye@users.noreply.github.com>
2024-08-01 13:54:03 +0300

    perf common: add color themes from figma
    color access control

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,20 @@ const config: Config = {
     extend: {
       colors: {
         'support-separator': '#33FFFFFF',
+        'support-overlay' : '#52000000',
+        'label-primary': '#FFFFFF',
+        'label-secondary': '#99FFFFFF',
+        'label-tertiary': '#66FFFFFF',
+        'label-disable': '#26FFFFFF',
+        'color-red': '#FF453A',
+        'color-green': '#32D74B',
+        'color-blue': '#0A84FF',
+        'color-grey': '#8E8E93',
+        'color-gray-light': '#48484A',
+        'color-white': '#FFFFFF',
+        'back-primary': '#161618',
+        'back-secondary': '#252528',
+        'back-elevated': '#3C3C3F',
       }
     },
   },
